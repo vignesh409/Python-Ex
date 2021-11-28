a0dbababc0da95b627dbe97a0bdf7844fa89bd50
vignesh <74180426+vignesh409@users.noreply.github.com>
2021-11-28 14:11:16 +0530

    Create Deathdayscal.py

---
+++ Deathdayscal.py
@@ -0,0 +1,10 @@
+print("Welcome to Remaining Death Days Calculator!!\U0001f923")
+age=input("Please Enter your Age: ")
+age_1=int(age)
+year=90-age_1
+days=year*365
+weeks=year*52
+months=year*12
+message=(f"You have {days} days,{weeks} weeks, {months} months to live!!")
+print(message)
+print("Just live your own life and Enjoy\U0001f902")
